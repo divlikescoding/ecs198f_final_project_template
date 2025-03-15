3a2c75a8f31c5f8ebcb7fcd8f15335555a312de4
Divyansh Rajesh Jain <drajeshjain@ucdavis.edu>
2025-03-15 15:40:33 -0700

    Test Multi-File imports

--- pychess/logic/chess_logic.py
+++ pychess/logic/chess_logic.py
@@ -1,4 +1,11 @@
-from .test import convert_square_to_index
+def convert_square_to_index(self, square: str) -> tuple[int, int]:
+    letter = square[0]
+    number = int(square[1])
+
+    row_index = 8 - number
+    column_index = ord(letter) - ord("a")
+
+    return (row_index, column_index)
 
 class ChessLogic:
     def __init__(self):
--- pychess/logic/test.py
+++ pychess/logic/test.py
@@ -1,9 +1,1 @@
 
-def convert_square_to_index(self, square: str) -> tuple[int, int]:
-    letter = square[0]
-    number = int(square[1])
-
-    row_index = 8 - number
-    column_index = ord(letter) - ord("a")
-
-    return (row_index, column_index)
